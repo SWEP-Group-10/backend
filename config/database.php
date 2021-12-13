f350be7f84bb1b194503920cd1ae6935365ffa95
Ore A <oreadeleye67@gmail.com>
2021-12-13 07:25:32 +0100

    commit fix db strings

--- config/database.php
+++ config/database.php
@@ -3,6 +3,9 @@
 use Doctrine\DBAL\Types\BooleanType;
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url('postgres://smmlexvppudboc:7f0db180cfd8f02b82656a4fac7ab611d34fd1ab70a
+a276420c9831cac2b41a0@ec2-52-0-196-50.compute-1.amazonaws.com:5432/d8n323i1trvt34');
+
 return [
     /*
     |--------------------------------------------------------------------------
@@ -65,17 +68,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
+            'url' => env('DATABASE_URL'),
+            'prefix_indexes' => true,
         ],
 
         'sqlsrv' => [
